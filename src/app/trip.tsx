dd1680c35cf6b1489d4d7c238101227cd1b75d1f
Kunal Sonawane <2kunalsonawane@gmail.com>
2024-02-22 15:24:41 -0800

    Add basic map markers

--- src/app/trip.tsx
+++ src/app/trip.tsx
@@ -1,14 +1,43 @@
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
-import ActionSheet from '@/components/utils/action-sheet';
-import BottomSheet, { BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import { useCallback, useRef, useState } from 'react';
-import Filter from '@/components/filter';
-import NavBar from '@/components/utils/navbar';
-import Prompt from '@/components/prompt';
-import MapView from 'react-native-maps';
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import ActionSheet from "@/components/utils/action-sheet";
+import BottomSheet, {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
+import { useCallback, useRef, useState } from "react";
+import { useLocalSearchParams, useGlobalSearchParams, Link } from "expo-router";
+
+import Filter from "@/components/filter";
+import NavBar from "@/components/utils/navbar";
+import Prompt from "@/components/prompt";
+import MapView, { Callout, Marker } from "react-native-maps";
+
+const markers = [
+	// San Francisco
+	{
+		latitude: 37.7749,
+		longitude: -122.4194,
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+		name: 'San Francisco City Center'
+	},
+	{
+		latitude: 37.8077,
+		longitude: -122.475,
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+		name: 'Golden Gate Bridge'
+	}
+];
 
 export default function Trip() {
-  const [navbarScr, setNavbarScr]  = useState(1);
+  const { data } = useLocalSearchParams();
+  console.log("params", data, typeof data);
+  // const mapData = JSON.parse(params?.data ?? "{}");
+  // console.log(params, mapData);
+
+  // console.log(mapData, typeof mapData);
+  const [navbarScr, setNavbarScr] = useState(1);
   const bottomSheetRef = useRef<BottomSheetModal>(null);
   const handleOpenPress = (scrNo: number) => {
     bottomSheetRef.current?.present();
@@ -25,41 +54,56 @@ export default function Trip() {
           latitudeDelta: 1,
           longitudeDelta: 2,
         }}
-      />
-      <View className='absolute bottom-14 self-center'>
-        <NavBar onPress={handleOpenPress}/>
+        showsUserLocation
+        showsMyLocationButton
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            title={marker.name}
+            coordinate={marker}
+          >
+            <Callout>
+              <View style={{ padding: 10 }}>
+                <Text style={{ fontSize: 24 }}>Hello</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+      <View className="absolute bottom-14 self-center">
+        <NavBar onPress={handleOpenPress} />
       </View>
       <ActionSheet
         ref={bottomSheetRef}
         index={0}
-        children={
-          (navbarScr == 1) ? <Prompt/> : <Filter/>
-        }/>
-  </View>
+        children={navbarScr == 1 ? <Prompt /> : <Filter />}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FEF4D9'
+    backgroundColor: "#FEF4D9",
   },
   contentContainer: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: "center",
   },
   buttonContainer: {
-    position: 'absolute',
-    bottom: '10%',
-    alignSelf: 'center',
-    zIndex: 1000
+    position: "absolute",
+    bottom: "10%",
+    alignSelf: "center",
+    zIndex: 1000,
   },
   button: {
-    backgroundColor: '#EC988D',
+    backgroundColor: "#EC988D",
   },
   image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover', // or 'stretch' or 'contain' as needed
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover", // or 'stretch' or 'contain' as needed
   },
 });
--- src/components/prompt.tsx
+++ src/components/prompt.tsx
@@ -1,51 +1,65 @@
-import React, { useState } from 'react';
-import Textarea from '../components/utils/textarea';
-import { View, StyleSheet, Text, Pressable } from 'react-native';
-import Button from './utils/button';
-import { useBottomSheet } from '@gorhom/bottom-sheet';
-import { Link } from 'expo-router';
+import React, { useState } from "react";
+import Textarea from "../components/utils/textarea";
+import { View, StyleSheet, Text, Pressable } from "react-native";
+import Button from "./utils/button";
+import { useBottomSheet } from "@gorhom/bottom-sheet";
+import { Link } from "expo-router";
+import { router } from "expo-router";
 
 export default function Prompt() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const { close: closePrompt } = useBottomSheet();
 
-  const Input = (innerRef: any) => {
-    return (
-      <Link href='/trip' asChild>
-        <Button 
-          title="Generate"
-          color='#EC988D'
-          onPress={() => {}}/>
-      </Link>
-    )
-  }
+  const generateTrip = async () => {
+    try {
+      const response = await fetch(
+        "https://7542-68-101-122-32.ngrok-free.app/maps/restaurants"
+      );
+      // const response = await fetch(
+      //   "https://7542-68-101-122-32.ngrok-free.app/maps/textsearch",
+      //   {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify({ text }),
+      //   }
+      // );
+
+      const data = await response.json();
+      console.log(data);
+
+      router.push({ pathname: "/trip", params: { data: data.results } });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <View style={styles.container}>
-        <Textarea
-          placeholder='Enter Trip prompt...'
-          text={text}
-          onChangeText={(t: string) => setText(t)} />
-        <View style={styles.row}>
+      <Textarea
+        placeholder="Enter Trip prompt..."
+        text={text}
+        onChangeText={(t: string) => setText(t)}
+      />
+      <View style={styles.row}>
+        <Pressable
+          className="bg-powder-gold w-48 flex items-center justify-center px-4 py-4 rounded-3xl"
+          onPress={() => closePrompt()}
+        >
+          <Text className="font-rethink text-2xl color-egg-white">Cancel</Text>
+        </Pressable>
+        <Link href="/trip" asChild>
           <Pressable
-            className="bg-powder-gold w-48 flex items-center justify-center px-4 py-4 rounded-3xl"
-            onPress={() => closePrompt()}
+            className="bg-mintish  w-48 flex items-center justify-center px-4 py-4 rounded-3xl"
+            onPress={generateTrip}
           >
-            <Text className="font-rethink text-2xl color-egg-white">
-              Cancel
+            <Text className="font-rethink text-2xl color-dark-text">
+              Generate!
             </Text>
           </Pressable>
-          <Link href='/trip' asChild>
-            <Pressable
-              className="bg-mintish  w-48 flex items-center justify-center px-4 py-4 rounded-3xl"
-              // onPress={() => {}}
-            >
-              <Text className="font-rethink text-2xl color-dark-text">
-                Generate!
-              </Text>
-            </Pressable>
-          </Link>
-        </View>
+        </Link>
+      </View>
     </View>
   );
 }
@@ -53,13 +67,13 @@ export default function Prompt() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    color: '#1E1E1E',
-    padding: 20
+    color: "#1E1E1E",
+    padding: 20,
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 20,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
